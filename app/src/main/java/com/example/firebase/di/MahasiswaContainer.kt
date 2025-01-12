efbe8140ac872fc22d9ea45c9eec579756aa12a4
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 16:01:23 +0700

    Menambahkan AppContainer dan MahasiswaContainer untuk mengelola repository mahasiswa

--- app/src/main/java/com/example/firebase/di/MahasiswaContainer.kt
+++ app/src/main/java/com/example/firebase/di/MahasiswaContainer.kt
@@ -1,5 +1,6 @@
 package com.example.firebase.di
 
+
 import com.example.firebase.repository.MahasiswaRepository
 import com.example.firebase.repository.NetworkMahasiswaRepository
 import com.google.firebase.firestore.FirebaseFirestore

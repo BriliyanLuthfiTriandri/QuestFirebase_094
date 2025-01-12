106a88a7bc8aa621a010a342634611b31ec97cdb
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 16:14:28 +0700

    Menambahkan InsertViewModel untuk mengelola state form, validasi, dan penyimpanan data mahasiswa

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -9,7 +9,61 @@ import com.example.firebase.model.Mahasiswa
 import com.example.firebase.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
 
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan inputan pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+
+    // Validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else " NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else " Nama tidak boleh kosong",
+            jenis_kelamin = if (event.jenis_kelamin.isNotEmpty()) null else " Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else " Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else " Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else " Angkatan tidak boleh kosong",
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun insertMhs(){
+        if (validateFields()) {
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMahasiswa(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data Berhasil Disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data Gagal Disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data Tidak Valid")
+        }
+    }
+
+    fun resetForm(){
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+    fun resetSnackBarMessage(){
+        uiState = FormState.Idle
+    }
+}
 
 
 sealed class FormState {

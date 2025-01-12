471be72efa9c9a8c1b6e85855f0716fe3d8261e1
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 16:09:40 +0700

    Menambahkan fungsi toMhsModel untuk mengonversi MahasiswaEvent menjadi Mahasiswa dan menyiman input form ke dalam entity

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.firebase.ui.viewmodel
+
+
+// Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenis_kelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)

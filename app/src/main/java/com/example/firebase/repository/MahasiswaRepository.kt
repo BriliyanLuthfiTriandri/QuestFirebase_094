ba433b161df3e7c1e90f11019483177ed72cf68f
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 16:03:06 +0700

    Menambahkan interface MahasiswaRepository untuk operasi CRUD mahasiswa

--- app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/firebase/repository/MahasiswaRepository.kt
@@ -5,12 +5,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository{
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
-
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-
     suspend fun updateMahasiswa(nim : String, mahasiswa: Mahasiswa)
-
     suspend fun deleteMahasiswa(nim : String)
-
     suspend fun getMahasiswaByID( nim: String): Flow<Mahasiswa?>
 }

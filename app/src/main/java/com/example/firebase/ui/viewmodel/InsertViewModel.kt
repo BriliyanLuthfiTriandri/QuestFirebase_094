25042d35087accbbde9dfb23c1764aa16627bf2a
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 16:10:33 +0700

    Menambahkan data class MahasiswaEvent untuk menyimpan data input form mahasiswa

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.firebase.ui.viewmodel
 
 
+
+
+// data class variable yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 // Menyimpan input form ke dalam entity
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,

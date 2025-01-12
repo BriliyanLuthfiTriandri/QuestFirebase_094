e5150bae15817225f815939be658adb74d4f1a08
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-12 18:23:03 +0700

    Menambahkan fungsi untuk menghapus data mahasiswa dengan penanganan error

--- app/src/main/java/com/example/firebase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/HomeViewModel.kt
@@ -41,6 +41,15 @@ class HomeViewModel(
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(mahasiswa)
+            } catch (e: Exception){
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
 
 sealed class HomeUiState{

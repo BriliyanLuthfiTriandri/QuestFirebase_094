2e55f795cacdc21f5eff3d33f227ce7e1b48d5c0
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2025-01-15 13:48:06 +0700

    Menampilkan judul_skripsi pada halaman home di setiap card

--- app/src/main/java/com/example/firebase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/firebase/ui/view/HomeView.kt
@@ -196,11 +196,11 @@ fun MhsCard(
                 )
             }
             Text(
-                text = mahasiswa.kelas,
+                text = mahasiswa.alamat,
                 style = MaterialTheme.typography.titleMedium
             )
             Text(
-                text = mahasiswa.alamat,
+                text = mahasiswa.judul_skripsi,
                 style = MaterialTheme.typography.titleMedium
             )
         }
